3f12a24f32cf6f3c0ee29e0de430bf0b7b701adc
martin_margonari <martinmargonari@gmail.com>
2016-09-24 00:20:33 -0300

    Added Course for choose Activity. Including Photo, Name, Description, Units and Professors

---
+++ app/src/main/java/com/example/margonari/tdp2_frontend/activities/CourseChooseActivity.java
@@ -0,0 +1,80 @@
+package com.example.margonari.tdp2_frontend.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.margonari.tdp2_frontend.R;
+import com.example.margonari.tdp2_frontend.adapters.CoursesAdapter;
+import com.example.margonari.tdp2_frontend.adapters.ProfessorAdapter;
+import com.example.margonari.tdp2_frontend.adapters.UnitAdapter;
+import com.example.margonari.tdp2_frontend.domain.Course;
+import com.example.margonari.tdp2_frontend.domain.Professor;
+import com.example.margonari.tdp2_frontend.domain.Unit;
+
+import java.net.PortUnreachableException;
+import java.util.ArrayList;
+
+public class CourseChooseActivity extends AppCompatActivity {
+
+    private RecyclerView unitsRecyclerView;
+    private RecyclerView.Adapter unitsAdapter;
+    private RecyclerView.LayoutManager unitsLayoutManager;
+
+    private RecyclerView professorsRecyclerView;
+    private RecyclerView.Adapter professorsAdapter;
+    private RecyclerView.LayoutManager professorsLayoutManager;
+    
+    private static String LOG_TAG = "CourseChooseActivity";
+    private  ArrayList<Unit> unitsList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_choose);
+
+        Intent intent = getIntent();
+        //unitsList = getDataSet();
+
+
+        unitsRecyclerView = (RecyclerView) findViewById(R.id.recycler_view_units);
+        unitsRecyclerView.setHasFixedSize(true);
+        unitsLayoutManager = new LinearLayoutManager(this);
+        unitsRecyclerView.setLayoutManager(unitsLayoutManager);
+        unitsRecyclerView.setFocusable(false);
+        unitsAdapter = new UnitAdapter(getDataSetUnits());
+        unitsRecyclerView.setAdapter(unitsAdapter);
+
+
+        professorsRecyclerView = (RecyclerView) findViewById(R.id.recycler_view_professors);
+        professorsRecyclerView.setHasFixedSize(true);
+        professorsLayoutManager = new LinearLayoutManager(this);
+        professorsRecyclerView.setLayoutManager(professorsLayoutManager);
+        professorsRecyclerView.setFocusable(false);
+        professorsAdapter = new ProfessorAdapter(getDataSetProfessors());
+        professorsRecyclerView.setAdapter(professorsAdapter);
+    }
+
+    private ArrayList<Unit> getDataSetUnits() {
+        ArrayList results = new ArrayList<Unit>();
+        for (int index = 0; index < 5; index++) {
+            Unit obj = new Unit("Nombre de Unidad", R.drawable.arte);
+            results.add(obj);
+        }
+        return results;
+
+    }
+
+    private ArrayList<Professor> getDataSetProfessors() {
+        ArrayList results = new ArrayList<Professor>();
+        results.add(new Professor("Nombre Profesor", true));
+        for (int index = 0; index < 5; index++) {
+            Professor obj = new Professor("Nombre Asistente", false);
+            results.add(obj);
+        }
+        return results;
+
+    }
+}
---
+++ app/src/main/java/com/example/margonari/tdp2_frontend/adapters/ProfessorAdapter.java
@@ -0,0 +1,91 @@
+package com.example.margonari.tdp2_frontend.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.margonari.tdp2_frontend.R;
+import com.example.margonari.tdp2_frontend.domain.Professor;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Margonari on 23/09/2016.
+ */
+public class ProfessorAdapter extends RecyclerView
+        .Adapter<ProfessorAdapter
+        .ProfessorHolder> {
+    private static String LOG_TAG = "ProfessorAdapter";
+    private ArrayList<Professor> mDataset;
+    private static MyClickListener myClickListener;
+
+    public static class ProfessorHolder extends RecyclerView.ViewHolder
+            implements View
+            .OnClickListener {
+        TextView professor_name;
+        TextView professor_type;
+        //ImageView Professor_photo;
+
+        public ProfessorHolder(View itemView) {
+            super(itemView);
+            professor_name = (TextView) itemView.findViewById(R.id.professor_name);
+            professor_type = (TextView) itemView.findViewById(R.id.professor_type);
+            //Professor_photo = (ImageView) itemView.findViewById(R.id.professor_photo);
+            Log.i(LOG_TAG, "Adding Listener");
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            myClickListener.onItemClick(getAdapterPosition(), v);
+        }
+    }
+
+    public void setOnItemClickListener(MyClickListener myClickListener) {
+        this.myClickListener = myClickListener;
+    }
+
+    public ProfessorAdapter(ArrayList<Professor> myDataset) {
+        mDataset = myDataset;
+    }
+
+    @Override
+    public ProfessorHolder onCreateViewHolder(ViewGroup parent,
+                                         int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.course_professor, parent, false);
+
+        ProfessorHolder dataObjectHolder = new ProfessorHolder(view);
+        return dataObjectHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ProfessorHolder holder, int position) {
+        holder.professor_name.setText(mDataset.get(position).getName());
+        holder.professor_type.setText(mDataset.get(position).getType());
+        //holder.professor_photo.setImageResource(mDataset.get(position).getPhoto_id());
+    }
+
+    public void addItem(Professor Professor, int index) {
+        mDataset.add(index, Professor);
+        notifyItemInserted(index);
+    }
+
+    public void deleteItem(int index) {
+        mDataset.remove(index);
+        notifyItemRemoved(index);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    public interface MyClickListener {
+        public void onItemClick(int position, View v);
+    }
+}
