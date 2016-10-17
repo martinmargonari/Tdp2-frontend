57bc5fc647e5f62db249a5b9d4e0d630efb2511a
martin_margonari <martinmargonari@gmail.com>
2016-10-16 21:31:53 -0300

    Logic in MyCourseUnit

--- app/src/main/java/com/example/margonari/tdp2_frontend/activities/MyCourseUnitActivity.java
+++ app/src/main/java/com/example/margonari/tdp2_frontend/activities/MyCourseUnitActivity.java
@@ -5,13 +5,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 
 import com.example.margonari.tdp2_frontend.R;
+import com.example.margonari.tdp2_frontend.adapters.CoursesAdapter;
 import com.example.margonari.tdp2_frontend.adapters.MaterialAdapter;
+import com.example.margonari.tdp2_frontend.domain.Course;
 import com.example.margonari.tdp2_frontend.domain.Material;
+import com.example.margonari.tdp2_frontend.domain.Question;
 import com.example.margonari.tdp2_frontend.domain.UnityInfo;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class MyCourseUnitActivity extends AppCompatActivity {
 
@@ -20,6 +26,9 @@ public class MyCourseUnitActivity extends AppCompatActivity {
     private RecyclerView.Adapter materialAdapter;
     private String api_token;
     private UnityInfo unityInfo;
+    private ArrayList<Material> materialList;
+    private static String LOG_TAG = "MyCourseUnitActivity";
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +50,44 @@ public class MyCourseUnitActivity extends AppCompatActivity {
     private ArrayList<Material> getDataSetMaterial() {
         //TODO
         ArrayList results = new ArrayList<Material>();
+        for (int i = 0; i < unityInfo.getMaterials().length; i++) {
+            results.add(unityInfo.getMaterials()[i]);
+        }
+
+        results.add(new Material("Examen",Material.EXAMEN));
 
         return results;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ((MaterialAdapter) materialAdapter).setOnItemClickListener(new MaterialAdapter
+                .MyClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
+                Material material = materialList.get(position);
+                int type = material.getType();
+
+                if (type == Material.DOCUMENTO) {
+                    //Abrir Documento
+                } else if (type == Material.VIDEO) {
+                        //Abrir Video
+                } else {
+                    //Abrir Examen
+                    System.out.println("GOT HERE");
+                    ArrayList questions = new ArrayList<Question>();
+                    for (int i = 0; i < unityInfo.getQuestions().length; i++) {
+                        questions.add(unityInfo.getQuestions()[i]);
+                    }
+                    Intent intent = new Intent(MyCourseUnitActivity.this,EvaluationActivity.class);
+                    intent.putExtra("API_TOKEN",api_token);
+                    intent.putExtra("QUESTIONS",questions);
+                    startActivity(intent);
+                }
+
+                Log.i(LOG_TAG, " Clicked on Item " + position);
+            }
+        });
     }
 }
