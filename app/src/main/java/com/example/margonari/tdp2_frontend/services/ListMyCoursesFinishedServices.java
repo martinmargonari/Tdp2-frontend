31d90c915308870b4a8469f0d8d710ba055f0342
aranlu27 <aran.com.ar@gmail.com>
2016-11-04 17:20:32 -0300

    se agrega servicio de cursos finalizados, y se arregla bug en inicio de sesion

---
+++ app/src/main/java/com/example/margonari/tdp2_frontend/services/ListMyCoursesFinishedServices.java
@@ -0,0 +1,41 @@
+package com.example.margonari.tdp2_frontend.services;
+
+import com.example.margonari.tdp2_frontend.domain.Course;
+import com.example.margonari.tdp2_frontend.domain.MyCourseBox;
+import com.example.margonari.tdp2_frontend.rest_dto.CourseBoxDTO;
+
+import java.util.ArrayList;
+
+/**
+ * Created by luis on 26/09/16.
+ */
+
+public class ListMyCoursesFinishedServices extends AbstractServices{
+
+    private static final String service_name = "courses/finished";
+
+    public ArrayList<Course> getListCoursesBy() {
+        String coursesQuery = this.getQueryBy();
+        System.out.println("QUERY " + coursesQuery.toString());
+        CourseBoxDTO coursesDTO = (CourseBoxDTO) geDataOftDTO(coursesQuery, CourseBoxDTO.class);
+        ArrayList<Course> listCourses= new ArrayList<>( );
+
+        ArrayList<MyCourseBox> listCourseLoco= coursesDTO.getData()[0];
+        for(  MyCourseBox myCourse_box : listCourseLoco){
+            myCourse_box.getCourse().setSession_id(myCourse_box.getId());
+            listCourses.add( myCourse_box.getCourse());
+        }
+        return listCourses;
+    }
+
+    @Override
+    protected String getQueryBy(String... params) {
+        String url = urlBase;
+        StringBuffer urlStringBuffer = new StringBuffer(url);
+        urlStringBuffer.append(service_name);
+        urlStringBuffer.append("?");
+        urlStringBuffer.append("api_token=");
+        urlStringBuffer.append(api_security);
+        return urlStringBuffer.toString();
+    }
+}
