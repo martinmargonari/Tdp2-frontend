3142e6aa7df2ba5a31ffd88da443323447d8606d
Luis <aran.com.ar@gmail.com>
2016-11-12 16:03:11 -0300

    Se carga las imagenes de las unidades

--- app/src/main/java/com/example/margonari/tdp2_frontend/adapters/MyCourseUnitAdapter.java
+++ app/src/main/java/com/example/margonari/tdp2_frontend/adapters/MyCourseUnitAdapter.java
@@ -1,6 +1,7 @@
 package com.example.margonari.tdp2_frontend.adapters;
 
 import android.content.Context;
+import android.media.Image;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,10 +13,17 @@ import android.widget.TextView;
 import com.example.margonari.tdp2_frontend.R;
 import com.example.margonari.tdp2_frontend.activities.MyCourseActivity;
 import com.example.margonari.tdp2_frontend.activities.MyCourseParentActivity;
+import com.example.margonari.tdp2_frontend.domain.Course;
 import com.example.margonari.tdp2_frontend.domain.ForumPost;
 import com.example.margonari.tdp2_frontend.domain.Unit;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.logging.Logger;
 
@@ -80,21 +88,31 @@ public class MyCourseUnitAdapter extends RecyclerView
     }
 
     @Override
-    public void onBindViewHolder(UnitHolder holder, int position) {
+    public void onBindViewHolder(final UnitHolder holder, int position) {
         holder.unit_name.setText(mDataset.get(position).getName());
         //TODO Agregar Descripcion de la unidad
 
-        if(mDataset.get(position).getFull_image()!=null){
-           /* String urlImage = holder.context.getResources().getString(R.string.imagesURL) + mDataset.get(position).getCourse_id() + "." + mDataset.get(position).getFile_extension();
-            Picasso.with(holder.context).load(urlImage).into(holder.unit_photo);
-            Log.d("URL_IMAGE_UNIT", urlImage);
+        String urlImage_unity=null;
+        String urlImage_course=null;
 
-            Picasso.with(holder.context).load(urlImage).into(holder.unit_photo);*/
+        Course course=((MyCourseParentActivity)mContext).getCourseFullData();
+        if((course)!=null && course.getUnities()!=null){
+            Unit unit= course.getUnities().get(position);
+            if (unit!=null & unit.getFull_image()!=null)
+            urlImage_unity = unit.getFull_image();
 
-        }else {
-            String urlImage = ((MyCourseParentActivity)mContext).getCourseFullData().getFull_image();
-            Log.d("URL_IMAGE_COURSE", urlImage);
-            Picasso.with(holder.context).load(urlImage).into(holder.unit_photo);
+        } else{
+            urlImage_course = course.getFull_image();
+
+        }
+
+        if(urlImage_unity!=null) {
+            Picasso.with(holder.context).load(urlImage_unity).into(holder.unit_photo);
+            Log.d("URL_IMAGE_UNIT", urlImage_unity);
+
+        }else if(urlImage_course!=null){
+            Log.d("URL_IMAGE_COURSE", urlImage_course);
+            Picasso.with(holder.context).load(urlImage_course).into(holder.unit_photo);
         }
         holder.week_number.setText("SEMANA " + Integer.toString(position+1));
         holder.week_limit.setText(mDataset.get(position).getExam_deadline().substring(0,10));
@@ -118,4 +136,6 @@ public class MyCourseUnitAdapter extends RecyclerView
     public interface MyClickListener {
         public void onItemClick(int position, View v);
     }
+
+
 }
