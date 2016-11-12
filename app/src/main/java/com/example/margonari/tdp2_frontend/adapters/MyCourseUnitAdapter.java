55f0ad781bc3dbf0d2fcc0b904035f2c6b5b143d
martin_margonari <martinmargonari@gmail.com>
2016-11-12 16:51:59 -0300

    My Course better UX

--- app/src/main/java/com/example/margonari/tdp2_frontend/adapters/MyCourseUnitAdapter.java
+++ app/src/main/java/com/example/margonari/tdp2_frontend/adapters/MyCourseUnitAdapter.java
@@ -74,6 +74,7 @@ public class MyCourseUnitAdapter extends RecyclerView
     @Override
     public void onBindViewHolder(UnitHolder holder, int position) {
         holder.unit_name.setText(mDataset.get(position).getName());
+        //String urlImage = holder.context.getResources().getString(R.string.imagesURL) + mDataset.get(position).getCourse_id() + "." + mDataset.get(position).getFile_extension();
         String urlImage = holder.context.getResources().getString(R.string.imagesURL) + mDataset.get(position).getCourse_id() + "." + mDataset.get(position).getFile_extension();
         Picasso.with(holder.context).load(urlImage).into(holder.unit_photo);
         holder.week_number.setText("SEMANA " + Integer.toString(position+1));
