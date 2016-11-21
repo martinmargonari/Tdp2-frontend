26f7823da5d16dfe32d4ba0aa8169a3b9888cc0f
Luis <aran.com.ar@gmail.com>
2016-11-21 03:45:45 -0300

    Se arregla problema al cerrar sesion

--- app/src/main/java/com/example/margonari/tdp2_frontend/activities/MainActivity.java
+++ app/src/main/java/com/example/margonari/tdp2_frontend/activities/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.margonari.tdp2_frontend.activities;
 
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -35,12 +34,10 @@ import com.example.margonari.tdp2_frontend.services.ForumCategoriesServices;
 import com.example.margonari.tdp2_frontend.services.ForumPostServices;
 import com.example.margonari.tdp2_frontend.services.ForumThreadsServices;
 import com.example.margonari.tdp2_frontend.services.ListCoursesByCategoriesServices;
-import com.example.margonari.tdp2_frontend.services.ListMyCoursesServices;
 import com.example.margonari.tdp2_frontend.services.LoginServices;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -56,24 +53,25 @@ public class MainActivity extends AppCompatActivity
 
     private static final String TAG = "MainActivity";
     private static final int RC_SIGN_IN = 0;
-
+    private ImageView imageProfile;
     private GridView grillaCategorias;
     private ImageAdapter adapterCategorias;
-    private String  api_token;
+    private String api_token;
     private String userEmail;
     private String firstName;
     private String lastName;
     private String profilePicture;
     private FirebaseAuth auth;
     private GoogleApiClient client;
-    ImageView imageProfile;
-
-
+    private Context context;
+    private TextView userNameText;
+    private TextView emailText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        context= this;
         this.setTitle("Encontrar Cursos");
         if (getIntent().getExtras() != null) {
             for (String key : getIntent().getExtras().keySet()) {
@@ -82,27 +80,6 @@ public class MainActivity extends AppCompatActivity
             }
         }
 
-        auth = FirebaseAuth.getInstance();
-        if ( auth.getCurrentUser()!=null){
-
-            Log.d("AUTH", "User LOGGED IN");
-            userEmail=auth.getCurrentUser().getEmail();
-            firstName=auth.getCurrentUser().getDisplayName();
-            Log.d("hola",auth.getCurrentUser().getProviders().toString());
-            auth.getCurrentUser().getProviderData();
-            profilePicture= auth.getCurrentUser().getPhotoUrl().toString();
-            InitApiTokenFromServer(userEmail);
-        }else{
-            startActivityForResult(
-                    AuthUI.getInstance().createSignInIntentBuilder()
-                            .setLogo(R.drawable.ic_launcher)
-                            .setIsSmartLockEnabled(false)
-                            .setProviders(
-                    AuthUI.FACEBOOK_PROVIDER,
-                    AuthUI.GOOGLE_PROVIDER
-            ).build(),RC_SIGN_IN);
-
-        }
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -118,23 +95,15 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
         View headerView = navigationView.getHeaderView(0);
 
-        TextView userNameText = (TextView) headerView.findViewById(R.id.user_name);
-        TextView emailText = (TextView) headerView.findViewById(R.id.user_email);
-         imageProfile = (ImageView) headerView.findViewById(R.id.profile_picture);
+         userNameText = (TextView) headerView.findViewById(R.id.user_name);
+         emailText = (TextView) headerView.findViewById(R.id.user_email);
+        imageProfile = (ImageView) headerView.findViewById(R.id.profile_picture);
 
-        userNameText.setText(firstName );
-        emailText.setText(userEmail);
+
         imageProfile.setImageDrawable(getDrawable(R.drawable.com_facebook_profile_picture_blank_portrait));
 
-        System.out.println("Profile pic: " +profilePicture);
 
-        if (profilePicture != null){
-            try{
-                Glide.with(this).load(profilePicture).into(imageProfile);
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
+
         grillaCategorias = (GridView) findViewById(R.id.grilla_categorias);
         adapterCategorias = new ImageAdapter(this);
         grillaCategorias.setAdapter(adapterCategorias);
@@ -144,43 +113,50 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
 
-        if( requestCode == RC_SIGN_IN){
-            if ( resultCode== RESULT_OK){
-                Log.d("RESULT","On activity result");
-                Log.d("Nombre usuario",auth.getCurrentUser().getDisplayName().toString());
-                profilePicture= auth.getCurrentUser().getPhotoUrl().toString();
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Log.d("RESULT", "On activity result");
+                Log.d("Nombre usuario", auth.getCurrentUser().getDisplayName().toString());
+                auth = FirebaseAuth.getInstance();
 
-                if (profilePicture != null){
-                    try{
-                        Glide.with(this).load(profilePicture).into(imageProfile);
-                    }catch (Exception e){
-                        e.printStackTrace();
+                if(imageProfile!=null &auth!=null & auth.getCurrentUser()!=null){
+                    if(auth.getCurrentUser().getPhotoUrl()!=null){
+                        profilePicture = auth.getCurrentUser().getPhotoUrl().toString();
+                        if (profilePicture != null) {
+                            try {
+                                Picasso.with(this).load(profilePicture).into(imageProfile);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
                     }
                 }
-                userEmail=auth.getCurrentUser().getEmail();
+                userEmail = auth.getCurrentUser().getEmail();
                 InitApiTokenFromServer(userEmail);
-                firstName=auth.getCurrentUser().getDisplayName().toString();
+                firstName = auth.getCurrentUser().getDisplayName().toString();
 
-            }else{
+            } else {
 
-                    Log.d("AUTHS","Problema con smart lock");
-                }
+                Log.d("AUTHS", "Problema con smart lock");
+            }
 
-        }else{Log.d("AUTH","User not autenticated");}
+        } else {
+            Log.d("AUTH", "User not autenticated");
+        }
     }
 
     private void InitApiTokenFromServer(String userEmail) {
         HttpRequestTaskLogin httpRequestTask = new HttpRequestTaskLogin();
 
         httpRequestTask.execute(userEmail, FirebaseInstanceId.getInstance().getToken(),
-                auth.getCurrentUser().getPhotoUrl().toString(),auth.getCurrentUser().getDisplayName());
+                auth.getCurrentUser().getPhotoUrl().toString(), auth.getCurrentUser().getDisplayName());
         try {
             Login login = (Login) httpRequestTask.get();
-            api_token=login.getApi_token();
+            api_token = login.getApi_token();
             CourselandApp.setApi_token(api_token);
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -190,21 +166,14 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-
     @Override
     public void onBackPressed() {
-        /*DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }*/
     }
 
     @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            getMenuInflater().inflate(R.menu.general_menu, menu);
-            return true;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.general_menu, menu);
+        return true;
     }
 
     @Override
@@ -235,57 +204,61 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.ajustes) {
             Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
             startActivity(intent);
-        } else if (id == R.id.cerrar_sesion) {
+        } else {
+            if (id == R.id.cerrar_sesion) {
+                if (auth.getCurrentUser() != null) {
+                    AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Log.d("AUTH", "User LOGGED OUT");
+                            FirebaseAuth.getInstance().signOut();
 
-            if ( auth.getCurrentUser()!=null){
-                AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        Log.d("AUTH", "User LOGGED OUT");
-                      /*  startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
-                                .setLogo(R.drawable.ic_launcher)
-                                .setProviders(
-                                        AuthUI.FACEBOOK_PROVIDER,
-                                        AuthUI.GOOGLE_PROVIDER
-                        ).build(),RC_SIGN_IN);*/
-                        FirebaseAuth.getInstance().signOut();
-                        finish();
-                        startActivity(getIntent());
-                    }
-                });
-            }
-
-            /*else{
-                startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
-                        .setLogo(R.drawable.ic_launcher)
-                        .setProviders(
-                                AuthUI.FACEBOOK_PROVIDER,
-                                AuthUI.GOOGLE_PROVIDER
-                        ).build(),RC_SIGN_IN);
-
+                        }
+                    });
+                }
                 finish();
-
-            }*/
+                startActivity(getIntent());
+            }
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-@Override
+
+    @Override
     public void onStart() {
         super.onStart();
         client.connect();
         Action viewAction = Action.newAction(
                 Action.TYPE_VIEW,
                 "Main Page",
-                 Uri.parse("http://host/path"),
+                Uri.parse("http://host/path"),
                 Uri.parse("android-app://com.example.margonari.tdp2_frontend/http/host/path")
         );
         AppIndex.AppIndexApi.start(client, viewAction);
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            Log.d("AUTH", "User LOGGED IN");
+            userEmail = auth.getCurrentUser().getEmail();
+            firstName = auth.getCurrentUser().getDisplayName();
+            InitApiTokenFromServer(userEmail);
+        } else {
+            startActivityForResult(
+                    AuthUI.getInstance().createSignInIntentBuilder()
+                            .setLogo(R.drawable.ic_launcher)
+                            .setIsSmartLockEnabled(false)
+                            .setProviders(
+                                    AuthUI.FACEBOOK_PROVIDER,
+                                    AuthUI.GOOGLE_PROVIDER
+                            ).build(), RC_SIGN_IN);
+
+        }
+        userNameText.setText(firstName);
+        emailText.setText(userEmail);
     }
 
-@Override
+    @Override
     public void onStop() {
         super.onStop();
         Action viewAction = Action.newAction(
@@ -299,34 +272,31 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-
     public void startActivity(Intent intent) {
-        // check if search intent
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             intent.putExtra("API_TOKEN", api_token);
         }
-
         super.startActivity(intent);
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Categoria item = (Categoria) parent.getItemAtPosition(position);
-            int item_id= Categoria.getCategoryByIdView(item.getId());
-            String category_name = item.getName();
+        int item_id = Categoria.getCategoryByIdView(item.getId());
+        String category_name = item.getName();
 
 
-        HttpRequestTask  httpRequestTask= new HttpRequestTask();
+        HttpRequestTask httpRequestTask = new HttpRequestTask();
         httpRequestTask.execute(String.valueOf(item_id));
         try {
-            ArrayList<Course> courses= (ArrayList<Course>) httpRequestTask.get();
+            ArrayList<Course> courses = (ArrayList<Course>) httpRequestTask.get();
             Intent intent = new Intent(this, CoursesActivity.class);
             intent.putExtra("API_TOKEN", api_token);
             intent.putExtra("LIST_COURSES", courses);
-            intent.putExtra("CATEGORY_NAME",category_name);
-            startActivity( intent);
+            intent.putExtra("CATEGORY_NAME", category_name);
+            startActivity(intent);
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -336,9 +306,9 @@ public class MainActivity extends AppCompatActivity
         protected ArrayList<Course> doInBackground(String... params) {
             try {
                 String id_categorie = params[0];
-                ListCoursesByCategoriesServices listCoursesByCategoriesServices= new ListCoursesByCategoriesServices();
+                ListCoursesByCategoriesServices listCoursesByCategoriesServices = new ListCoursesByCategoriesServices();
                 listCoursesByCategoriesServices.setApi_security(api_token);
-                ArrayList<Course>  listCourses= (ArrayList<Course>) listCoursesByCategoriesServices.getListCoursesBy(id_categorie);
+                ArrayList<Course> listCourses = (ArrayList<Course>) listCoursesByCategoriesServices.getListCoursesBy(id_categorie);
 
                 return listCourses;
             } catch (Exception e) {
@@ -355,13 +325,13 @@ public class MainActivity extends AppCompatActivity
         protected Login doInBackground(String... params) {
             try {
                 String user = params[0];
-                String token= params[1];
-                String image_url=params[2];
-                String name=params[3];
+                String token = params[1];
+                String image_url = params[2];
+                String name = params[3];
 
                 Log.d("tokenRequestLogin", token);
 
-                Login login=  new LoginServices().getLoginBy(user, token,image_url,name);
+                Login login = new LoginServices().getLoginBy(user, token, image_url, name);
                 return login;
             } catch (Exception e) {
                 Log.e("LoginActivity", e.getMessage(), e);
@@ -372,71 +342,8 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-
-
-    private class HttpRequestTaskForumCategories extends AsyncTask<String, Void, ArrayList<ForumCategory>> {
-
-        ArrayList<ForumCategory> listaCategorias;
-        @Override
-        protected ArrayList<ForumCategory> doInBackground(String... params) {
-            try {
-                String course_id = params[0];
-
-                ForumCategoriesServices forumCategoriesServices= new ForumCategoriesServices();
-                forumCategoriesServices.setApi_security(api_token);
-                listaCategorias= (ArrayList<ForumCategory>) forumCategoriesServices.getListCategoriesBy(course_id);
-                return listaCategorias;
-            } catch (Exception e) {
-                Log.e("LoginActivity", e.getMessage(), e);
-            }
-
-            return null;
-        }
-
+    @Override
+    protected void onDestroy() {
+       super.onDestroy();
     }
-
-    private class HttpRequestTaskForumThreads extends AsyncTask<String, Void, ArrayList<ForumThread>> {
-
-        ArrayList<ForumThread> listaThreads;
-        @Override
-        protected ArrayList<ForumThread> doInBackground(String... params) {
-            try {
-                String category_id = params[0];
-
-                ForumThreadsServices forumCategoriesServices= new ForumThreadsServices();
-                forumCategoriesServices.setApi_security(api_token);
-                listaThreads = (ArrayList<ForumThread>) forumCategoriesServices.getListThreadsBy(category_id);
-                return listaThreads;
-            } catch (Exception e) {
-                Log.e("LoginActivity", e.getMessage(), e);
-            }
-
-            return null;
-        }
-
-    }
-
-
-    private class HttpRequestTaskForumPost extends AsyncTask<String, Void, ArrayList<ForumPost>> {
-
-        ArrayList<ForumPost> listaPost;
-        @Override
-        protected ArrayList<ForumPost> doInBackground(String... params) {
-            try {
-                String thread_id = params[0];
-
-                ForumPostServices forumPostServices= new ForumPostServices();
-                forumPostServices.setApi_security(api_token);
-                listaPost = (ArrayList<ForumPost>) forumPostServices.getListPostBy(thread_id);
-               Log.d("EL POST :",listaPost.get(0).toString()) ;
-                return listaPost;
-            } catch (Exception e) {
-                Log.e("LoginActivity", e.getMessage(), e);
-            }
-
-            return null;
-        }
-
-    }
-
 }
--- app/src/main/java/com/example/margonari/tdp2_frontend/activities/SettingsActivity.java
+++ app/src/main/java/com/example/margonari/tdp2_frontend/activities/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.example.margonari.tdp2_frontend.activities;
 
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
@@ -18,11 +19,13 @@ import com.example.margonari.tdp2_frontend.services.NotificationsOnOffServices;
 import java.util.ArrayList;
 
 public class SettingsActivity extends AppCompatActivity {
-
+    public static final String PREFS_NAME = "MyPrefsFile";
+    boolean hasNotifications;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -30,8 +33,16 @@ public class SettingsActivity extends AppCompatActivity {
 
         //TODO Leer desde el servidor y setear el isChecked del switch
 
+        // Restore preferences
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+         hasNotifications = settings.getBoolean("hasNotifications", true);
+
+        switchNotifications.setChecked(hasNotifications);
+
         //Set a Click Listener for Switch Button
         switchNotifications.setOnClickListener(new View.OnClickListener(){
+
+
             @Override
             public void onClick(View v){
                 boolean on = ((Switch) v).isChecked();
@@ -85,5 +96,21 @@ public class SettingsActivity extends AppCompatActivity {
         }
 
     }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        // We need an Editor object to make preference changes.
+        // All objects are from android.context.Context
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean("hasNotifications", hasNotifications);
+
+        // Commit the edits!
+        editor.commit();
+    }
+
+
 
 }
