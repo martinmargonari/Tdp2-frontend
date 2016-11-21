154247e7bd5e6ef43c3a43f6548f24e57a63f97d
Luis <aran.com.ar@gmail.com>
2016-11-21 14:08:41 -0300

    Fix: Categorias vacias error fatal

--- app/src/main/java/com/example/margonari/tdp2_frontend/activities/MainActivity.java
+++ app/src/main/java/com/example/margonari/tdp2_frontend/activities/MainActivity.java
@@ -99,7 +99,7 @@ public class MainActivity extends AppCompatActivity
         }
 
 
-
+        initMenu();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
