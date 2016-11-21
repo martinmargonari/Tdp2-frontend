da47cc70c0891378588b822fae2d50e607bbcead
Luis <aran.com.ar@gmail.com>
2016-11-21 07:15:12 -0300

    Se agrega refresco de activities

--- app/src/main/java/com/example/margonari/tdp2_frontend/adapters/ForumThreadAdapter.java
+++ app/src/main/java/com/example/margonari/tdp2_frontend/adapters/ForumThreadAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,16 +13,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.margonari.tdp2_frontend.R;
+import com.example.margonari.tdp2_frontend.activities.MyCourseForumThreadActivity;
 import com.example.margonari.tdp2_frontend.domain.ForumThread;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
-import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
 /**
@@ -34,10 +28,93 @@ public class ForumThreadAdapter extends RecyclerView
         .Adapter<ForumThreadAdapter
         .ForumThreadHolder> {
     private static String LOG_TAG = "ForumThreadAdapter";
-    private ArrayList<ForumThread> mDataset;
     private static ForumThreadAdapter.MyClickListener myClickListener;
+    private ArrayList<ForumThread> mDataset;
     private Context mCOntext;
 
+
+    public ForumThreadAdapter(ArrayList<ForumThread> myDataset, Context context) {
+        mDataset = myDataset;
+        mCOntext = context;
+
+    }
+
+    public void setOnItemClickListener(ForumThreadAdapter.MyClickListener myClickListener) {
+        ForumThreadAdapter.myClickListener = myClickListener;
+    }
+
+    @Override
+    public ForumThreadAdapter.ForumThreadHolder onCreateViewHolder(ViewGroup parent,
+                                                                   int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.forum_thread, parent, false);
+
+        ForumThreadAdapter.ForumThreadHolder dataObjectHolder = new ForumThreadAdapter.ForumThreadHolder(view);
+        return dataObjectHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final ForumThreadAdapter.ForumThreadHolder holder, int position) {
+        holder.thread_author.setText(mDataset.get(position).getAuthor().getName());
+        Picasso.with(mCOntext).load(R.drawable.profile_pic_user).into(holder.thread_author_pic);
+
+        Picasso.Builder builder = new Picasso.Builder(this.mCOntext);
+        builder.listener(new Picasso.Listener() {
+            @Override
+            public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+        builder.build().load(mDataset.get(position).getAuthor_image()).into(
+
+                new Target() {
+                    @Override
+                    public void onBitmapLoaded(final Bitmap bitmap, Picasso.LoadedFrom from) {
+                        holder.thread_author_pic.setImageBitmap(bitmap);
+
+                    }
+                    @Override
+                    public void onPrepareLoad(Drawable placeHolderDrawable) {
+                    }
+
+                    @Override
+                    public void onBitmapFailed(Drawable errorDrawable) {
+
+                    }
+                }
+        );
+
+
+        //TODO Nombre de autor e imagen de autor
+        holder.thread_title.setText(mDataset.get(position).getTitle());
+        holder.thread_replies.setText(mDataset.get(position).getReply_count());
+        holder.thread_date.setText(mDataset.get(position).getCreated_at());
+        /*git stif(holder.thread_author_pic.get((R.drawable.profile_pic_user))){
+            ((MyCourseForumThreadActivity)mCOntext).resetActivity();
+
+        }*/
+
+
+    }
+
+    public void addItem(ForumThread forumThread, int index) {
+        mDataset.add(index, forumThread);
+        notifyItemInserted(index);
+    }
+
+    public void deleteItem(int index) {
+        mDataset.remove(index);
+        notifyItemRemoved(index);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    public interface MyClickListener {
+        void onItemClick(int position, View v);
+    }
 
     public static class ForumThreadHolder extends RecyclerView.ViewHolder
             implements View
@@ -67,92 +144,5 @@ public class ForumThreadAdapter extends RecyclerView
         public void onClick(View v) {
             myClickListener.onItemClick(getAdapterPosition(), v);
         }
-    }
-
-    public void setOnItemClickListener(ForumThreadAdapter.MyClickListener myClickListener) {
-        this.myClickListener = myClickListener;
-    }
-
-    public ForumThreadAdapter(ArrayList<ForumThread> myDataset, Context context) {
-        mDataset = myDataset;
-        mCOntext= context;
-
-    }
-
-    @Override
-    public ForumThreadAdapter.ForumThreadHolder onCreateViewHolder(ViewGroup parent,
-                                                                   int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.forum_thread, parent, false);
-
-        ForumThreadAdapter.ForumThreadHolder dataObjectHolder = new ForumThreadAdapter.ForumThreadHolder(view);
-        return dataObjectHolder;
-    }
-
-    @Override
-    public void onBindViewHolder(final ForumThreadAdapter.ForumThreadHolder holder, int position) {
-        holder.thread_author.setText(mDataset.get(position).getAuthor().getName());
-        Picasso.with(mCOntext).load( R.drawable.profile_pic_user).into(holder.thread_author_pic);
-
-        Picasso.Builder builder = new Picasso.Builder(this.mCOntext);
-        builder.listener(new Picasso.Listener()
-        {
-            @Override
-            public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception)
-            {
-                exception.printStackTrace();
-            }
-        });
-        builder.build().load(mDataset.get(position).getAuthor_image()).into(
-
-                new Target() {
-                    @Override
-                    public void onBitmapLoaded (final Bitmap bitmap, Picasso.LoadedFrom from){
-
-                        holder.thread_author_pic.setImageBitmap(bitmap);
-
-                    }
-
-                    @Override
-                    public void onPrepareLoad(Drawable placeHolderDrawable) {
-
-
-                    }
-
-                    @Override
-                    public void onBitmapFailed(Drawable errorDrawable) {
-                        Picasso.with(mCOntext).load( R.drawable.profile_pic_user).into(holder.thread_author_pic);
-
-                    }
-                }
-        );
-
-
-
-
-
-        //TODO Nombre de autor e imagen de autor
-        holder.thread_title.setText(mDataset.get(position).getTitle());
-        holder.thread_replies.setText(mDataset.get(position).getReply_count());
-        holder.thread_date.setText(mDataset.get(position).getCreated_at());
-    }
-
-    public void addItem(ForumThread forumThread, int index) {
-        mDataset.add(index, forumThread);
-        notifyItemInserted(index);
-    }
-
-    public void deleteItem(int index) {
-        mDataset.remove(index);
-        notifyItemRemoved(index);
-    }
-
-    @Override
-    public int getItemCount() {
-        return mDataset.size();
-    }
-
-    public interface MyClickListener {
-        public void onItemClick(int position, View v);
     }
 }
